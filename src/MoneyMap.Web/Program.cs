65e36814b1e0ff62f9e196cba9f41dcbcc447b69
Hakim Ghods <ghods.hakim@hotmail.com>
2025-03-12 14:07:58 +0330

    db from server

--- src/MoneyMap.Web/Program.cs
+++ src/MoneyMap.Web/Program.cs
@@ -1,10 +1,11 @@
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using MoneyMap.Application;
 using MoneyMap.Application.Services;
 using MoneyMap.Infrastructure.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -44,6 +45,9 @@ builder.Services.AddRazorPages().AddRazorPagesOptions(options =>
 builder.Services.AddTransient<IExpenseService, ExpenseService>();
 builder.Services.AddTransient<ICalendarService, CalendarService>();
 builder.Services.AddTransient<IUserService, UserService>();
+
+builder.WebHost.UseStaticWebAssets();
+
 
 var app = builder.Build();
 
