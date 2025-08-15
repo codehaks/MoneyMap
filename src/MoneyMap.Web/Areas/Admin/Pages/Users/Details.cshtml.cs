d86fa50e33bfaa66e548628aa3bcb3ebde4af4dc
Hakim Ghods <ghods.hakim@hotmail.com>
2025-08-15 23:30:49 +0330

    Admin User Details

--- src/MoneyMap.Web/Areas/Admin/Pages/Users/Details.cshtml.cs
+++ src/MoneyMap.Web/Areas/Admin/Pages/Users/Details.cshtml.cs
@@ -2,12 +2,15 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MoneyMap.Application;
+using MoneyMap.Infrastructure.Data;
 
 namespace MoneyMap.Web.Areas.Admin.Pages.Users;
 
 public class DetailsModel : PageModel
 {
-    public IList<string> Roles { get; set; }
+    public IList<string> Roles { get; set; } = new List<string>();
+    public ApplicationUser? UserDetails { get; set; }
+    public string UserId { get; set; } = string.Empty;
 
     private readonly IUserService _userService;
 
@@ -18,7 +21,9 @@ public class DetailsModel : PageModel
 
     public async Task<PageResult> OnGet(string id)
     {
-        Roles =await _userService.GetRoles(id);
+        UserId = id;
+        Roles = await _userService.GetRoles(id);
+        UserDetails = _userService.GetAll().FirstOrDefault(u => u.Id == id);
         return Page();
     }
 }
